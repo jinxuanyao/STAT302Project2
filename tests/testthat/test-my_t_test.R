0f685061875d76a096f51aa12fb498c25549d575
jinxuan yao <yaojinxuan0@gmail.com>
2020-12-15 14:30:59 -0800

    add 2 more tests

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -6,3 +6,11 @@ test_that("my_t_test only includes specified input for alternative", {
 test_that("my t.test returns a list", {
   expect_is(my_t_test(x = 1:100, alternative = "two.sided", mu = 1),"list")
 })
+
+test_that("my t.test returns a list", {
+  expect_is(my_t_test(x = 1:100, alternative = "less", mu = 1),"list")
+})
+
+test_that("my t.test returns a list", {
+  expect_is(my_t_test(x = 1:100, alternative = "greater", mu = 1),"list")
+})
